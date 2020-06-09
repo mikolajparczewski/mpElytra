7ff797006f8fabfbfc2a780dded72385c5af4224
Mikołaj Parczewski <50463807+mikolajparczewski@users.noreply.github.com>
2020-06-10 00:11:15 +0200

    Update EntityDeath.java

--- src/me/mparczewski/mpElytra/EntityDeath.java
+++ src/me/mparczewski/mpElytra/EntityDeath.java
@@ -18,6 +18,6 @@ public class EntityDeath implements Listener {
 	
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent e) {
-		if((e.getEntity().getType() == EntityType.SHULKER) && (new Random().nextInt(99) <= 49)) e.getDrops().add(new ItemStack(Material.ELYTRA, 1));
+		if((e.getEntity().getType() == EntityType.SHULKER) && (new Random().nextInt(100) < 10)) e.getDrops().add(new ItemStack(Material.ELYTRA, 1));
 	}
 }
