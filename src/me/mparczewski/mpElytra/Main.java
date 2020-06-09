4f4f61d7cb8677bc88dd00175311ee9a287f89da
Mikołaj Parczewski <50463807+mikolajparczewski@users.noreply.github.com>
2020-06-10 00:11:33 +0200

    Update Main.java

--- src/me/mparczewski/mpElytra/Main.java
+++ src/me/mparczewski/mpElytra/Main.java
@@ -1,6 +1,6 @@
 /**
  * mpElytra
- * Copyright © 2019 Mikołaj Parczewski
+ * Copyright © 2020 Mikołaj Parczewski
  */
 
 package me.mparczewski.mpElytra;
@@ -11,11 +11,8 @@ public class Main extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
-		
 		getServer().getPluginManager().registerEvents(new ProjectileHit(), this);
 		getServer().getPluginManager().registerEvents(new CreatureSpawn(), this);
 		getServer().getPluginManager().registerEvents(new EntityDeath(), this);
-		
 	}
-
 }
